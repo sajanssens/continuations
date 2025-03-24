44153f0019984801f02ffa05b45b77d00f9cbb08
Bram Janssens <s.a.janssens@gmail.com>
2025-03-24 15:51:41 +0100

    More comments.

--- src/main/java/com/balarawool/continuations/virtualthread/VirtualThreadDemo.java
+++ src/main/java/com/balarawool/continuations/virtualthread/VirtualThreadDemo.java
@@ -8,15 +8,15 @@ public class VirtualThreadDemo {
         for (int i = 0; i < 1000; i++) {
             var vt1 = new VirtualThread(() -> { // TODO 1: show VirtualThread
                 System.out.println("1.1");
-                System.out.println("1.2");
-                WaitingOperation.perform("Network", 2); // 👈🏼 should perform the operation, pause now, resume in ... seconds
+                System.out.println("1.2");              // TODO 2 show:
+                WaitingOperation.perform("Network", 2); // 👈🏼 should perform the operation, suspend now, resume in ... seconds
                 System.out.println("1.3");
                 System.out.println("1.4");
             });
             var vt2 = new VirtualThread(() -> {
                 System.out.println("2.1");
                 System.out.println("2.2");
-                WaitingOperation.perform("DB", 5);     // 👈🏼 should perform the operation, pause now, resume in ... seconds
+                WaitingOperation.perform("DB", 5);     // 👈🏼 should perform the operation, suspend now, resume in ... seconds
                 System.out.println("2.3");
                 System.out.println("2.4");
             });
@@ -25,7 +25,7 @@ public class VirtualThreadDemo {
             SCHEDULER.schedule(vt2); // 👈🏼 should start the VT
         }
 
-        // TODO 2: show VirtualThreadScheduler
+        // TODO 3: show VirtualThreadScheduler
         VirtualThreadScheduler.start(SCHEDULER);
     }
 }
